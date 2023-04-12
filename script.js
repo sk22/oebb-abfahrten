d7ed6b54e7fd32016a62116e8b875dbff0c59933
Glitch (glitch-hello-website) <none>
2023-04-12 12:21:14 +0000

    ❣️🎒 Checkpoint ./index.html:1578515/86 ./script.js:1578515/2587 ./style.css:1578515/396

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const ul = document.getElementById('bahnhoefe')
+const table = document.getElementById('bahnhoefe')
+const operatorsDiv = document.getElementById('operators')
+
+// from https://meine.oebb.at/abfahrtankunft/webdisplay/config/config.js
+const operators = {
+  CAT: 'cat.jpg',
+  DB: 'db.jpg',
+  GKB: 'gkb.jpg',
+  LILO: 'lilo.jpg',
+  MBS: 'mbs.jpg',
+  MERIDIAN: 'meridian.jpg',
+  NOVOG: 'novog.jpg',
+  ÖBB: 'oebb.jpg',
+  RBC: 'rbc.jpg',
+  SLB: 'slb.jpg',
+  STB: 'stb.jpg',
+  STH: 'sth.jpg',
+  WALSER: 'walser.jpg',
+  WEST: 'west.jpg',
+  WLB: 'wlb.jpg',
+  ZB: 'zb.jpg',
+};
+
+Object.values(operators).forEach(op => {
+  const img = document.createElement('img')
+  img.src = `https://meine.oebb.at/abfahrtankunft/webdisplay/assets/operators/${op}.jpg`
+  
+})
 
 fetch('./at.csv').then(async res => {
   const csv = await res.text()
   console.log(csv)
   const bhfs = csv.split('\n').map(l => l.split(';'))
   bhfs.forEach(bhf => {
-    const li = document.createElement('li')
+    const tr = document.createElement('tr')
+    const bhfTd = document.createElement('td')
+    bhfTd.innerText = bhf[1];
+    tr.appendChild(bhfTd)
+
     const abfahrten = document.createElement('a')
     abfahrten.innerText = 'Abfahrten'
     abfahrten.href = `https://meine.oebb.at/abfahrtankunft/departure?evaNr=${bhf[0]}&static=true`
-    li.appendChild(abfahrten)
-    li.appendChild(document.createTextNode(' / '))
+    const abfahrtenTd = document.createElement('td')
+    abfahrtenTd.appendChild(abfahrten)
+    tr.appendChild(abfahrtenTd)
+    
     const ankuenfte = document.createElement('a')
     ankuenfte.innerText = 'Ankünfte'
     ankuenfte.href = `https://meine.oebb.at/abfahrtankunft/arrival?evaNr=${bhf[0]}&static=true`
-    li.appendChild(ankuenfte)
-    li.appendChild(document.createTextNode(` - ${bhf[1]}`))
-    ul.appendChild(li)
+    const ankuenfteTd = document.createElement('td')
+    ankuenfteTd.appendChild(ankuenfte)
+    tr.appendChild(ankuenfteTd)
+
+    table.appendChild(tr)
   })
 })
