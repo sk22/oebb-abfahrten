7b74fcb8328a214e29bcaee95240e53e079c40eb
Glitch (glitch-hello-website) <none>
2023-04-12 08:21:52 +0000

    🎎👙 Checkpoint ./index.html:1578515/701 ./script.js:1578515/2005 ./style.css:1578515/28

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+const ul = document.getElementById('bahnhoefe')
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
+fetch('./at.csv').then(async res => {
+  const csv = await res.text()
+  console.log(csv)
+  const bhfs = csv.split('\n').map(l => l.split(';'))
+  bhfs.forEach(bhf => {
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${bhf[1]))
+    const abfahrten = document.createElement('a')
+    abfahrten.innerText = 'Abfahrten'
+    abfahrten.href = `https://meine.oebb.at/abfahrtankunft/departure?evaNr=${bhf[0]}&static=true`
+    li.appendChild(abfahrten)
+    li.appendChild(document.createTextNode(' / '))
+    const ankuenfte = document.createElement('a')
+    ul.appendChild(li)
+  })
+})
