9e641bcd51ceec2eb4acf45bed45980084ccf8e3
sk22 <sk22@mailbox.org>
2026-02-11 17:37:40 +0100

    update links

--- generate.js
+++ generate.js
@@ -23,8 +23,7 @@ const tableHtml = oebbStations
 				<div class="station-info"><a class="station-name" href="https://infotogo.oebb.at/~${plc}~">${name}</a> <small>${plc}</small></div>
 				<div class="station-links">
 					<a href="https://meine.oebb.at/abfahrtankunft/departure/?plc=${plc}&static=false">Mo<wbr>bil</a>
-					<a href="https://meine.oebb.at/webdisplay/?stationId=${plc}&contentType=formation&staticLayout=true&ignoreIncident=true">Fo<wbr>rmation</a>
-					<a href="webdisplay.html?stationId=${plc}&contentType=departure">Ab<wbr>fahrten</a>
+					<a href="webdisplay.html?stationId=${plc}&contentType=departure&prepend=construction,formation">Ab<wbr>fahrten</a>
 					<a href="webdisplay.html?stationId=${plc}&contentType=arrival">An<wbr>künfte</a>
 				</div>
 			</div>`,
